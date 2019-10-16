cd71ff21fccb89c5c8b949d3a410b18d4f4d74f6
Stefany Cristina Oliveira De Souza <T804950@tokiomarine.com.br>
2019-10-16 18:26:39 -0300

    Projeto final - stock trader

---
+++ Vue/Vue JS 2/stock-trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'Apple', price: 180},
+    { id: 2, name: 'Google', price: 200},
+    { id: 3, name: 'BMW', price: 100},
+    { id: 4, name: 'Twitter', price: 67}
+]
---
+++ Vue/Vue JS 2/stock-trader/src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+
+import router from './router'
+import store from './store/store'
+
+Vue.config.productionTip = false
+
+Vue.filter('currency', value => {
+	const formatter = new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+		maximumFractionDigits: 2
+	})
+
+	return formatter.format(value)
+})
+
+new Vue({
+	router,
+	store,
+	render: h => h(App),
+}).$mount('#app')
